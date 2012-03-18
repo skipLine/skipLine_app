117d8e77e319e863444d16171ae999a81be0814f
skipLine <hong_cheng@berkeley.edu>
2012-03-18 15:07:06 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkipLineApp::Application.config.secret_token = '7ff92854279287f3d77423e13656faeacd23065d559935efd4172ed786c9e765eb1612a76c4e5cc50736031b9daed3d09fc092d115660491027381f84c338a02'
